c2fc8c531d184e14b35ab269d044368ab03f71cf
azam-akram <akram.muhammadazam@gmail.com>
2023-01-16 19:09:29 +0000

    just created springboot gradle project

---
+++ src/main/java/com/azamakram/github/springbootcassandradocker/SpringbootCassandraDockerApplication.java
@@ -0,0 +1,13 @@
+package com.azamakram.github.springbootcassandradocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCassandraDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCassandraDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azamakram/github/springbootcassandradocker/SpringbootCassandraDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azamakram.github.springbootcassandradocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCassandraDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
